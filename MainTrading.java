2c5ca387061f9fead7bf22fe89223262a29c5a26
blenkala <blenkala@asu.edu>
2022-10-19 23:50:10 -0700

    Added MainTrading class

---
+++ MainTrading.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+/* To initiate and call Facade*/
+public class MainTrading {
+    public static void main(String[] args) throws IOException {
+        Facade obj = new Facade();
+        obj.beginFacade();
+    }
+}
