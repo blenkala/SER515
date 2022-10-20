1ec3ef6d28d9b74866c58056d4b0d0f0678535ee
blenkala <blenkala@asu.edu>
2022-10-20 11:39:34 -0700

    Updated loginforValidation class

--- loginForValidation.java
+++ loginForValidation.java
@@ -1,14 +1,22 @@
 import java.io.*;
 public class loginForValidation {
-    public boolean validate(String uname, String password) throws IOException {
-        File file = new File(
-                "/Users/bhavanilenkala/SER515/composition_diagram/SER515-xyz/Database/BuyerInfo.txt");
-        BufferedReader br
-                = new BufferedReader(new FileReader(file));
-        String st;
-        while ((st = br.readLine()) != null){
-            String dbUsername=st.substring(0,4);
-            String dbPassword=st.substring(5);
+    public boolean validate(String sellerOrBuyer,String uname, String password) throws IOException {
+        BufferedReader br;
+        File file;
+        if(sellerOrBuyer.equals("0")) {
+             file = new File(
+                    "/Users/bhavanilenkala/SER515/composition_diagram/SER515-xyz/Database/BuyerInfo.txt");
+        }
+        else{
+             file = new File(
+                    "/Users/bhavanilenkala/SER515/composition_diagram/SER515-xyz/Database/SellerInfo.txt");
+        }
+        br = new BufferedReader(new FileReader(file));
+        String val;
+        while ((val = br.readLine()) != null){
+            String[] arrOfStr = val.split(":");
+            String dbUsername= arrOfStr[0];
+            String dbPassword=arrOfStr[1];
             if(dbUsername.equals(uname) && dbPassword.equals(password))
                  return true;
     }
